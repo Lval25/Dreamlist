17cde1027f6f1cb785643f3fd48e3d305dcbeae7
Loyd Vallot <lvallot25@gmail.com>
2017-06-28 19:33:06 -0500

    Final commmit

--- Dreamlist/Itemcell.swift
+++ Dreamlist/Itemcell.swift
@@ -2,7 +2,7 @@
 //  Itemcell.swift
 //  Dreamlist
 //
-//  Created by Loyd Vallot on 6/17/17.
+//  Created by Loyd Vallot on 6/28/17.
 //  Copyright © 2017 Loyd's Productions. All rights reserved.
 //
 
@@ -10,18 +10,15 @@ import UIKit
 
 class Itemcell: UITableViewCell {
 
-    @IBOutlet weak var thumb: UIImageView!
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var price: UILabel!
-    @IBOutlet weak var details: UILabel!
-    
-    
-    func configureCell(item: Item)
-    {
-        title.text = item.title
-        price.text = "$\(item.price)"
-        details.text = item.details
-        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
-   
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
